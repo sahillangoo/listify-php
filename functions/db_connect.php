e72800632c5524b54a2085a501a99046006010f3
Sahil Langoo <sahilahmed3066@gmail.com>
2023-08-13 19:09:28 +0530

    Added Prod Vars :rocket:

--- functions/db_connect.php
+++ functions/db_connect.php
@@ -19,14 +19,16 @@ error_reporting(E_ALL);
 // Timezone Configuration
 date_default_timezone_set('Asia/Kolkata');
 
-// const('ENV', 'PROD');
-const ENV = 'DEV';
-const DB_HOST = 'localhost';
-if (ENV === 'PROD') {
-    define('DB_NAME', 'db_name');
-    define('DB_USER', 'db_user');
-    define('DB_PASS', 'db_pass');
+$ENV = $_ENV('ENV');
+if ($ENV === 'PROD') {
+  $DB_HOST = $_ENV('AZURE_MYSQL_HOST');
+  $DB_PORT = $_ENV('AZURE_MYSQL_PORT');
+  $DB_NAME = $_ENV('AZURE_MYSQL_DBNAME');
+  $DB_USER = $_ENV('AZURE_MYSQL_USERNAME');
+  $DB_PASS = $_ENV('AZURE_MYSQL_PASSWORD');
 } else {
+  define('DB_HOST', 'localhost');
+  define('DB_PORT', '3306');
     define('DB_NAME', 'listify_db');
     define('DB_USER', 'root');
     define('DB_PASS', '');
@@ -37,15 +39,18 @@ function connectToDB()
 {
   try {
     // Create a new PDO instance
-    $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4";
+    $dsn = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=utf8mb4";
     $options = [
       PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
       PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
     ];
-      return new PDO($dsn, DB_USER, DB_PASS, $options);
-  } catch (Exception $e) {
-    error_log($e->getMessage());
+    //Create instance of PDO class
+    $pdo = new PDO($dsn, DB_USER, DB_PASS, $options);
+    //Return the PDO instance
+    return $pdo;
+  } catch (PDOException $e) {
+    // If there is an error with the connection, stop the script and display the error.
     exit('Something weird happened with DB || Is DB server turned on?'); //something a user can understand
   }
 }
