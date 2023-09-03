47581cd7d8f42d28516fb6295b8318f1fd3dcb47
Sahil Langoo <40671195+sahillangoo@users.noreply.github.com>
2023-09-03 21:49:28 +0530

    reworked on listing and added linter  :maple_leaf:

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,188 +1,212 @@
+/*
+main.js
+Author: Sahil Langoo
+File Content
+- truncate description
+- Tooltips
+- validation for forms
+- helper for adding on all elements multiple attributes
+- toggle password
+- search bar
+
+
+*/
 const debounce = (func, delay) => {
-	let timeoutId
-	return (...args) => {
-		clearTimeout(timeoutId)
-		timeoutId = setTimeout(() => {
-			func.apply(null, args)
-		}, delay)
-	}
-}
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
 
 document.addEventListener('DOMContentLoaded', () => {
-	// function for truncate description
-	const descriptions = document.querySelectorAll('#truncate')
-	descriptions.forEach((description) => {
-		description.textContent = description.textContent.slice(0, 120) + '...'
-	})
+  // function for truncate description
+  const descriptions = document.querySelectorAll('#truncate');
+  descriptions.forEach((description) => {
+    description.textContent = description.textContent.slice(0, 120) + '...';
+  });
 
-	// initialization of Tooltips
-	const tooltipTriggerList = [].slice.call(
-		document.querySelectorAll('[data-bs-toggle="tooltip"], [data-toggle="tooltip"]'),
-	)
-	const tooltipList = tooltipTriggerList.map((tooltipTriggerEl) => {
-		return new bootstrap.Tooltip(tooltipTriggerEl)
-	})
+  // initialization of Tooltips
+  const tooltipTriggerList = [].slice.call(
+    document.querySelectorAll(
+      '[data-bs-toggle="tooltip"], [data-toggle="tooltip"]'
+    )
+  );
+  const tooltipList = tooltipTriggerList.map((tooltipTriggerEl) => {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
 
-	// validation for forms
-	const inputs = [...document.querySelectorAll('input')]
-	inputs.forEach((input) => {
-		input.addEventListener(
-			'input',
-			debounce(async (event) => {
-				const regex = getRegexForInput(input)
-				await validateInput(input, regex)
-			}, 500),
-			false,
-		)
-	})
+  // validation for forms
+  const inputs = [...document.querySelectorAll('input')];
+  inputs.forEach((input) => {
+    input.addEventListener(
+      'input',
+      debounce(async (event) => {
+        const regex = getRegexForInput(input);
+        await validateInput(input, regex);
+      }, 500),
+      false
+    );
+  });
 
-	function validateInput(input, regex) {
-		const value = input.value.trim()
-		return Promise.resolve(regex.test(value))
-			.then((isValid) => {
-				input.classList.toggle('is-valid', isValid)
-				input.classList.toggle('is-invalid', !isValid)
-			})
-			.catch(console.error)
-	}
-	const regexPatterns = {
-		username: /^(?<username>[a-zA-Z0-9._]{6,20})$/,
-		email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-		phone: /^[0-9]{10}$/,
-		password:
-			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#\-_@$!%*?&+~|{}:;<>/])[A-Za-z\d#\-_@$!%*?&+~|{}:;<>/]{8,18}$/,
-		businessName: /^[a-zA-Z0-9 ]{3,20}$/,
-		businessAddress: /^[a-zA-Z0-9 ]{3,20}$/,
-		about: /^[a-zA-Z0-9 .,?!'":;()@#$%&*+-/]{10,200}$/,
-		pincode: /^[0-9]{6}$/,
-		facebookid: /^[a-zA-Z0-9._]{3,20}$/,
-		website:
-			/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-zA-Z0-9]+([a-zA-Z0-9-]+)?(\.[a-zA-Z0-9-]+)+([\/?].*)?$/,
-		gst: /^[0-9]{15}$/,
-		review: /^[a-zA-Z0-9 .,?!'":;()@#$%&*+-/]{10,150}$/,
-		rating: /^[1-5]{1}$/,
-	}
+  function validateInput(input, regex) {
+    const value = input.value.trim();
+    return Promise.resolve(regex.test(value))
+      .then((isValid) => {
+        input.classList.toggle('is-valid', isValid);
+        input.classList.toggle('is-invalid', !isValid);
+      })
+      .catch(console.error);
+  }
+  const regexPatterns = {
+    username: /^(?<username>[a-zA-Z0-9._]{6,20})$/,
+    email:
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+    phone: /^[0-9]{10}$/,
+    password:
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#\-_@$!%*?&+~|{}:;<>/])[A-Za-z\d#\-_@$!%*?&+~|{}:;<>/]{8,18}$/,
+    businessName: /^[a-zA-Z0-9 ]{3,20}$/,
+    businessAddress: /^[a-zA-Z0-9 ]{3,20}$/,
+    about: /^[a-zA-Z0-9 .,?!'":;()@#$%&*+-/]{10,200}$/,
+    pincode: /^[0-9]{6}$/,
+    facebookid: /^[a-zA-Z0-9._]{3,20}$/,
+    website:
+      /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-zA-Z0-9]+([a-zA-Z0-9-]+)?(\.[a-zA-Z0-9-]+)+([\/?].*)?$/,
+    gst: /^[0-9]{15}$/,
+    review: /^[a-zA-Z0-9 .,?!'":;()@#$%&*+-/]{10,150}$/,
+    rating: /^[1-5]{1}$/,
+  };
 
-	function getRegexForInput(input) {
-		return regexPatterns[input.name] || null
-	}
-})
+  function getRegexForInput(input) {
+    return regexPatterns[input.name] || null;
+  }
+});
 
 const inputGroupClickHandler = (event) => {
-	try {
-		const parent = event.target.closest('.input-group')
-		if (event.target.classList.contains('form-control')) {
-			const focus = document.querySelectorAll('.input-group.focused')
-			focus.forEach((el) => el.classList.remove('focused'))
-			parent.classList.add('focused')
-		}
-		const focus = document.querySelectorAll('.input-group.focused')
-		if (focus && event.target != parent && event.target.parentNode != parent) {
-			focus.forEach((el) => el.classList.remove('focused'))
-		}
-	} catch (error) {
-		console.error(error)
-	}
-}
+  try {
+    const parent = event.target.closest('.input-group');
+    if (event.target.classList.contains('form-control')) {
+      const focus = document.querySelectorAll('.input-group.focused');
+      focus.forEach((el) => el.classList.remove('focused'));
+      parent.classList.add('focused');
+    }
+    const focus = document.querySelectorAll('.input-group.focused');
+    if (focus && event.target != parent && event.target.parentNode != parent) {
+      focus.forEach((el) => el.classList.remove('focused'));
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 // helper for adding on all elements multiple attributes
 function setAttributes(el, options) {
-	try {
-		Object.keys(options).forEach((attr) => {
-			el.setAttribute(attr, options[attr])
-		})
-	} catch (error) {
-		console.error(error)
-	}
+  try {
+    Object.keys(options).forEach((attr) => {
+      el.setAttribute(attr, options[attr]);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function togglePassword() {
-	const password = document.querySelector('#password')
-	const toggle = document.querySelector('#toggle-password')
-	if (password.type === 'password') {
-		password.type = 'text'
-		toggle.classList.replace('fa-eye-slash', 'fa-eye')
-	} else {
-		password.type = 'password'
-		toggle.classList.replace('fa-eye', 'fa-eye-slash')
-	}
+  const password = document.querySelector('#password');
+  const toggle = document.querySelector('#toggle-password');
+  if (password.type === 'password') {
+    password.type = 'text';
+    toggle.classList.replace('fa-eye-slash', 'fa-eye');
+  } else {
+    password.type = 'password';
+    toggle.classList.replace('fa-eye', 'fa-eye-slash');
+  }
 }
 
 // search bar
-const searchInput = document.getElementById('search-input')
-const searchFeedback = document.getElementById('search-feedback')
-const searchResults = document.getElementById('search-results')
-const searchSpinner = document.getElementById('search-spinner')
+const searchInput = document.getElementById('search-input');
+const searchFeedback = document.getElementById('search-feedback');
+const searchResults = document.getElementById('search-results');
+const searchSpinner = document.getElementById('search-spinner');
 
 function displaySearchResults(results) {
-	searchResults.innerHTML = ''
-	searchFeedback.innerHTML = ''
-	if (Array.isArray(results) && results.length === 0 && searchInput.value.length >= 3) {
-		searchFeedback.innerHTML = `No results found for this query`
-		searchInput.classList.add('is-invalid')
-	} else if (Array.isArray(results)) {
-		results.slice(0, 6).forEach((result) => {
-			const resultElement = document.createElement('a')
-			resultElement.href = `./listing.php?listing=${result.id}`
-			resultElement.classList.add('list-group-item', 'list-group-item-action')
-			resultElement.innerHTML = `
+  searchResults.innerHTML = '';
+  searchFeedback.innerHTML = '';
+  if (
+    Array.isArray(results) &&
+    results.length === 0 &&
+    searchInput.value.length >= 3
+  ) {
+    searchFeedback.innerHTML = `No results found for this query`;
+    searchInput.classList.add('is-invalid');
+  } else if (Array.isArray(results)) {
+    results.slice(0, 6).forEach((result) => {
+      const resultElement = document.createElement('a');
+      resultElement.href = `./listing.php?listing=${result.id}`;
+      resultElement.classList.add('list-group-item', 'list-group-item-action');
+      resultElement.innerHTML = `
 				<div class="d-flex w-100 justify-content-between align-items-center">
-					<h5 class="text-gradient text-primary font-weight-bold h5 mb-1">${result.businessName}</h5>
+					<h5 class="text-gradient text-primary font-weight-bold h5 mb-1">${
+            result.businessName
+          }</h5>
 					<span class="text-body-secondary text-gradient text-warning text-uppercase text-xs mt-1"><i class="fa-solid fa-star"></i> ${
-						result.avg_rating?.toFixed(2) || 0
-					} (${result.reviews_count})</span>
+            result.avg_rating?.toFixed(2) || 0
+          } (${result.reviews_count})</span>
 					<span class="text-body-secondary text-capitalize text-xs font-weight-bold"><i class="fa-solid fa-shop"></i> ${
-						result.category
-					}</span>
+            result.category
+          }</span>
 					<span class="text-body-secondary text-capitalize text-xs font-weight-bold "><i class="fa-solid fa-location-dot"></i> ${
-						result.address
-					}, ${result.city}</span>
+            result.address
+          }, ${result.city}</span>
 				</div>
-			`
-			searchResults.appendChild(resultElement)
-		})
-		searchInput.classList.remove('is-invalid')
-	} else if (results.error) {
-		searchFeedback.innerHTML = `${results.error}`
-		searchInput.classList.add('is-invalid')
-	}
-	searchResults.classList.toggle('d-none', results.length === 0)
-	searchSpinner.classList.add('d-none')
+			`;
+      searchResults.appendChild(resultElement);
+    });
+    searchInput.classList.remove('is-invalid');
+  } else if (results.error) {
+    searchFeedback.innerHTML = `${results.error}`;
+    searchInput.classList.add('is-invalid');
+  }
+  searchResults.classList.toggle('d-none', results.length === 0);
+  searchSpinner.classList.add('d-none');
 }
 
 async function search() {
-	const searchQuery = searchInput.value.trim()
-	if (/^\d+$/.test(searchQuery)) {
-		searchFeedback.innerHTML = `Search query cannot contain only numbers`
-		searchInput.classList.add('is-invalid')
-		searchResults.classList.add('d-none')
-		searchSpinner.classList.add('d-none')
-		return
-	}
-	if (!/^[a-zA-Z]{3,20}$/.test(searchQuery)) {
-		searchFeedback.innerHTML = `Search query must be between 3 and 20 characters long and contain only letters`
-		searchInput.classList.add('is-invalid')
-		searchResults.classList.add('d-none')
-		searchSpinner.classList.add('d-none')
-		return
-	}
-	searchSpinner.classList.remove('d-none')
-	try {
-		const response = await fetch(`search.php?q=${searchQuery}`)
-		const data = await response.json()
-		displaySearchResults(data)
-	} catch (error) {
-		console.error(error)
-	}
+  const searchQuery = searchInput.value.trim();
+  if (/^\d+$/.test(searchQuery)) {
+    searchFeedback.innerHTML = `Search query cannot contain only numbers`;
+    searchInput.classList.add('is-invalid');
+    searchResults.classList.add('d-none');
+    searchSpinner.classList.add('d-none');
+    return;
+  }
+  if (!/^[a-zA-Z]{3,20}$/.test(searchQuery)) {
+    searchFeedback.innerHTML = `Search query must be between 3 and 20 characters long and contain only letters`;
+    searchInput.classList.add('is-invalid');
+    searchResults.classList.add('d-none');
+    searchSpinner.classList.add('d-none');
+    return;
+  }
+  searchSpinner.classList.remove('d-none');
+  try {
+    const response = await fetch(`search.php?q=${searchQuery}`);
+    const data = await response.json();
+    displaySearchResults(data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-searchInput.addEventListener('input', debounce(search, 500))
-document.getElementById('search-input').addEventListener('input', debounce(search, 500))
-document.addEventListener('click', inputGroupClickHandler, false)
+searchInput.addEventListener('input', debounce(search, 500));
+document
+  .getElementById('search-input')
+  .addEventListener('input', debounce(search, 500));
+document.addEventListener('click', inputGroupClickHandler, false);
 
 document.getElementById('clear-search-input').addEventListener('click', () => {
-	searchInput.value = ''
-	searchInput.classList.remove('is-invalid')
-	searchResults.innerHTML = ''
-	searchResults.classList.add('d-none')
-})
+  searchInput.value = '';
+  searchInput.classList.remove('is-invalid');
+  searchResults.innerHTML = '';
+  searchResults.classList.add('d-none');
+});
--- listing.php
+++ listing.php
@@ -30,25 +30,120 @@ if (!isset($_GET['listing']) || empty($_GET['listing']) || strlen($_GET['listing
 <head>
   <title><?php ?> </title>
   <?php include_once './includes/_head.php'; ?>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+  <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+  <style>
+    #mapid {
+      height: 400px;
+    }
+
+    .custom-radius {
+      stroke: #3388ff;
+      stroke-opacity: 0.8;
+      stroke-width: 2;
+      fill: #3388ff;
+      fill-opacity: 0.2;
+    }
+  </style>
+
+  <?php
+  // Initialize the test coordinates
+  $lat = 34.08468475517784;
+  $lng = 74.79748543611667;
+
+  // Output the JavaScript code using heredoc
+  echo <<<EOT
+  <script>
+  function initMap() {
+    // The location of the test coordinates
+    const testCoords = { lat: $lat, lng: $lng };
+    // The map, centered at the test coordinates
+    const map = L.map('mapid').setView(testCoords, 13);
+    // The marker, positioned at the test coordinates
+    L.marker(testCoords).addTo(map);
+    // The radius, positioned at the test coordinates
+    const radius = L.circle(testCoords, {
+      color: 'blue',
+      fillColor: '#3388ff',
+      fillOpacity: 0.2,
+      radius: 200, // in meters
+    }).addTo(map);
+    // The tile layer, using OpenStreetMap's standard tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+    }).addTo(map);
+  }
+  initMap();
+  </script>
+  EOT;
+  ?>
 </head>
 
-<body class="index-page">
+<body class="index-page" onload="initMap()">
   <?php
   // include the header file
   include_once './includes/_navbar.php';
   ?>
   <!-- Display the listing details using Bootstrap 5 -->
-  <div class="container">
+  <div class="container my-5">
+    <!-- breadcrumb -->
+    <div class="row pt-5 overflow-hidden">
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="./index.php">Home</a></li>
+          <li class="breadcrumb-item"><a href="./categories.php">Categories</a></li>
+          <li class="breadcrumb-item active text-capitalize" aria-current="page"><?php echo $result['category']; ?></li>
+        </ol>
+      </nav>
+    </div>
     <div class="row">
       <div class="col-md-6">
-        <img src="./uploads/business_images/<?php echo $result['displayImage']; ?>" class="img-fluid" alt="<?php echo $result['businessName']; ?>">
+        <img src="./uploads/business_images/<?php echo $result['displayImage']; ?>" class="img-fluid rounded move-on-hover border " alt="<?php echo $result['businessName']; ?>">
       </div>
       <div class="col-md-6">
-        <h1><?php echo $result['businessName']; ?></h1>
+        <h2 class="h2 text-gradient text-primary"><?php echo $result['businessName']; ?></h2>
+        <!-- rating -->
+        <p class="text-gradient text-warning text-3xl animate gradient-animation">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <span class="text-sm">(1)</span>
+        </p>
+
+
+        <p class="text-muted"><?php echo $result['category']; ?></p>
+        <p class="text-muted"><?php echo $result['createdAt']; ?></p>
+        <p class="text-muted"><?php echo $result['updatedAt']; ?></p>
+        <p class="text-muted"><?php echo $result['featured']; ?></p>
+        <p class="text-muted"><?php echo $result['active']; ?></p>
+        <p class="text-muted"><?php echo $result['user_id']; ?></p>
+
+        <p class="text-muted"><?php echo $result['reviews_count']; ?></p>
+        <p class="text-muted"><?php echo $result['username']; ?></p>
+        <p class="text-muted"><?php echo $result['businessName']; ?></p>
+        <p class="text-muted"><?php echo $result['description']; ?></p>
+        <p class="text-muted"><?php echo $result['phoneNumber']; ?></p>
+        <p class="text-muted"><?php echo $result['address']; ?></p>
+        <p class="text-muted"><?php echo $result['city']; ?></p>
+        <p class="text-muted"><?php echo $result['state']; ?></p>
+        <p class="text-muted"><?php echo $result['zip']; ?></p>
+        <p class="text-muted"><?php echo $result['country']; ?></p>
+        <p class="text-muted"><?php echo $result['website']; ?></p>
+        <p class="text-muted"><?php echo $result['facebook']; ?></p>
+        <p class="text-muted"><?php echo $result['twitter']; ?></p>
+        <p class="text-muted"><?php echo $result['instagram']; ?></p>
+
         <p><?php echo $result['description']; ?></p>
         <p>Contact: <?php echo $result['phoneNumber']; ?></p>
         <p>Address: <?php echo $result['address']; ?></p>
         <p>City: <?php echo $result['city']; ?></p>
+
+        <!-- map leaflet -->
+        <div id="mapid"></div>
+
 
       </div>
     </div>
